61ffc532d2a48cd91873fefd50bd854052de6839
CJVIK <carl.viklund@hotmail.com>
2024-12-23 14:40:23 +0100

    added

--- src/usercheckoutform.ts
+++ src/usercheckoutform.ts
@@ -16,17 +16,50 @@ formBilling.id = "billing-form";
 const nameInput = document.createElement("input");
 nameInput.type = "text";
 nameInput.name = "name";
-nameInput.placeholder = "Name";
+nameInput.placeholder = "First Name";
+
 // Last name input
 const lastNameInput = document.createElement("input");
 lastNameInput.type = "text";
 lastNameInput.name = "lastname";
 lastNameInput.placeholder = "Last Name";
+
 //Email input
 const emailInput = document.createElement("input");
 emailInput.type = "email";
 emailInput.name = "email";
 emailInput.placeholder = "Email";
+
+// Billing address input
+const billingAddressInput = document.createElement("input");
+billingAddressInput.type = "text";
+billingAddressInput.name = "billing-address";
+billingAddressInput.placeholder = "Billing Address";
+
+// Billing address line 2 input
+const billingAddressLine2Input = document.createElement("input");
+billingAddressLine2Input.type = "text";
+billingAddressLine2Input.name = "billing-address-line-2";
+billingAddressLine2Input.placeholder = "Billing Address Line 2";
+
+// Country input
+const countryInput = document.createElement("input");
+countryInput.type = "text";
+countryInput.name = "country";
+countryInput.placeholder = "Country";
+
+// City input
+const cityInput = document.createElement("input");
+cityInput.type = "text";
+cityInput.name = "city";
+cityInput.placeholder = "City";
+
+// Post address input
+const postAddressInput = document.createElement("input");
+postAddressInput.type = "text";
+postAddressInput.name = "post-address";
+postAddressInput.placeholder = "Post Address";
+
 // Append
 formUser.appendChild(nameInput);
 formUser.appendChild(lastNameInput);
