58cc5ac33fee940f5d5734c3b767397d021ab996
FrejaE <freja.edberg@outlook.com>
2025-01-02 13:17:44 +0100

    created HTML

---
+++ src/HTMLHelpers/orderOverview.ts
@@ -0,0 +1,59 @@
+import { Product } from "../Models/Product";
+
+const orderviewDiv = document.getElementById("orderOverview");
+const h3 = document.createElement("h3");
+h3.innerHTML = "Your order";
+
+export const products: Product[] = [
+  new Product(
+    "Shirt",
+    100,
+    "../../assets/products/Blue shirt.png",
+    "Blue Shirt",
+    "En stickad tröja med avslappnad passform tillverkad av en mjuk blandning av återvunnen polyamid och återvunnen polyester, utökad med en touch av återvunnen elastan och ull för extra stretch och värme. Den har en rund ribbad halsringning, droppade axlar och ribbade kanter.",
+    "abc123",
+    1
+  ),
+  new Product(
+    "Red dress",
+    250,
+    "../../assets/products/Red dress.png",
+    "Red Dress",
+    "Klänning med jacquardvävt mönster. Rundad midja med rynk. Dold dragkedja mitt bak. Fodrad upptill.",
+    "abc124",
+    1
+  ),
+  new Product(
+    "Boots",
+    300,
+    "../../assets/products/Boots.png",
+    "Boots",
+    "Ett par chelseaboots med resår i sidorna och hälla bak. Foder och innersula i satin.",
+    "abc125",
+    1
+  ),
+  new Product(
+    "Red bag",
+    250,
+    "../../assets/products/Red bag.png",
+    "Red bag",
+    "En crossbody-väska med två separata fack och dolt magnetlås. Väskan har reglerbar axelrem och reglerbart handtag med klätt spänne i vardera sida. Fodrad. Djup 9 cm. Höjd 23 cm. Bredd 26 cm",
+    "abc1237",
+    1
+  ),
+];
+const orderViewHMTL = () => {
+  products.forEach((product) => {
+    const img = document.createElement("img");
+    img.src = product.imgSrc;
+    img.alt = product.imgAlt;
+    const title = document.createElement("h4");
+    title.innerHTML = product.title;
+    const amount = document.createElement("p");
+    amount.innerHTML = "Amount: " + product.amount;
+    const sum = document.createElement("p");
+    sum.innerHTML = String(product.amount * product.price);
+    const totalSum = document.createElement("p");
+    totalSum.innerHTML = String(product.amount * product.price);
+  });
+};
