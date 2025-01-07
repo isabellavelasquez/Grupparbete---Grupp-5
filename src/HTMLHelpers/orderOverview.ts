cec796cea033449c605ac36d5b4b489f339ba022
isabellavelasquez <100156325+isabellavelasquez@users.noreply.github.com>
2025-01-07 11:47:53 +0100

    linked orderOverview page to local storage

--- src/HTMLHelpers/orderOverview.ts
+++ src/HTMLHelpers/orderOverview.ts
@@ -1,8 +1,6 @@
-import { Product } from "../Models/Product";
 import { cart, getProductFromID, loadCart } from "../services.ts/cartService";
 import "../styles/orderOverview.scss";
 import { theBackButton } from "./thebackbutton";
-import { products } from "./productsOverview";
 
 const cartIcon = document.getElementById("cartIcon");
 if (cartIcon) {
@@ -13,45 +11,40 @@ const orderviewDiv = document.getElementById("orderOverview");
 const h3 = document.createElement("h3");
 h3.innerHTML = "Your order";
 orderviewDiv?.appendChild(h3);
+let total = 0;
 
-const orderViewHMTL = (product: Product) => {
+const orderViewHMTL = () => {
   loadCart();
-  // const productsFromCart = Array.from(cart.entries());
-  const productAmount = cart.get(product.id) || 0;
 
-  // productsFromCart.forEach((productFromLocalS) => {
-  //   const productAmount = productFromLocalS[1];
-  //   const productID: string = productFromLocalS[0];
-  // const product = products.find((product) => product.id === productID);
-  for (const [id] of cart) {
-    const productID = getProductFromID(id);
+  for (const [id, amount] of cart.entries()) {
+    const product = getProductFromID(id);
 
-    // const product = getProductFromID(id);
-    if (productID) {
+    if (product) {
       const productsContainer = document.createElement("div");
       const img = document.createElement("img");
       img.src = product.imgSrc;
       img.alt = product.imgAlt;
       const title = document.createElement("h4");
       title.innerHTML = product.title;
-      const amount = document.createElement("p");
-      amount.innerHTML = "Amount: " + productAmount;
-      amount.classList.add("pTagAmount");
+      const quantity = document.createElement("p");
+      quantity.innerHTML = "Amount: " + amount;
+      quantity.classList.add("pTagAmount");
       const sum = document.createElement("p");
-      sum.innerHTML = String(productAmount * product.price) + " :-";
+      sum.innerHTML = String(amount * product.price) + " :-";
       sum.classList.add("sumPtag");
 
-      productsContainer.append(img, title, amount, sum);
+      productsContainer.append(img, title, quantity, sum);
       orderviewDiv?.appendChild(productsContainer);
+
     }
+    if(product) {
+    total += product.price * amount;
+    }
+
   }
   const totalSum = document.createElement("p");
   totalSum.classList.add("totalSum");
 
-  let total = 0;
-  products.forEach((product) => {
-    total += product.price * productAmount;
-  });
   const totalSumDiv = document.createElement("div");
   totalSumDiv.classList.add("sumDiv");
   const pTagTotal = document.createElement("p");
@@ -61,5 +54,5 @@ const orderViewHMTL = (product: Product) => {
   totalSumDiv.append(pTagTotal, totalSum);
   orderviewDiv?.appendChild(totalSumDiv);
 };
-orderViewHMTL(product);
+orderViewHMTL();
 theBackButton("../../assets/icons/back.arrow.png", "backbutton");
