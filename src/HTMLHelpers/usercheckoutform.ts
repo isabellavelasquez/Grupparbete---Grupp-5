143a68f6954333332743ca7a9cef2fa2b406b28c
CJVIK <carl.viklund@hotmail.com>
2025-01-08 10:14:45 +0100

    Refactor user checkout form to utilize validation service.ts for credit card input

--- src/HTMLHelpers/usercheckoutform.ts
+++ src/HTMLHelpers/usercheckoutform.ts
@@ -1,4 +1,9 @@
 import { createUserCheckoutForm } from "./userCheckoutFormHelper.ts";
+import {
+  addValidation,
+  isValidCreditCardNumber,
+  highlightCardType,
+} from "../services.ts/validationService.ts";
 
 document
   .getElementById("form-container")
@@ -8,58 +13,6 @@ const userForm = document.getElementById("user-form") as HTMLFormElement;
 userForm?.addEventListener("submit", (event) => {
   event.preventDefault();
 });
-function addValidation(
-  input: HTMLInputElement,
-  errorMessage: string,
-  validateInput: (value: string) => boolean
-) {
-  const customValidityMessage = document.createElement("p");
-  customValidityMessage.id = `${input.name}-validity-error`;
-  customValidityMessage.style.display = "none";
-  customValidityMessage.style.color = "red";
-  input.insertAdjacentElement("afterend", customValidityMessage);
-
-  input.addEventListener("input", () => {
-    if (input.value === "") {
-      input.classList.remove("invalid");
-      customValidityMessage.style.display = "none";
-    } else if (!validateInput(input.value)) {
-      input.classList.add("invalid");
-      customValidityMessage.textContent = errorMessage;
-      customValidityMessage.style.display = "block";
-    } else {
-      input.classList.remove("invalid");
-      customValidityMessage.style.display = "none";
-    }
-  });
-}
-
-function isValidCreditCardNumber(value: string): boolean {
-  const visa = /^4[0-9]{12}(?:[0-9]{3})?$/;
-  const masterCard = /^5[1-5][0-9]{14}$/;
-  return visa.test(value) || masterCard.test(value);
-}
-
-function highlightCardType(
-  value: string,
-  img1: HTMLImageElement,
-  img2: HTMLImageElement
-) {
-  const visa = /^4[0-9]{12}(?:[0-9]{3})?$/;
-  const masterCard = /^5[1-5][0-9]{14}$/;
-  if (visa.test(value)) {
-    img2.classList.add("highlight");
-    img1.classList.add("grayed-out");
-    img1.classList.remove("highlight");
-  } else if (masterCard.test(value)) {
-    img1.classList.add("highlight");
-    img2.classList.add("grayed-out");
-    img2.classList.remove("highlight");
-  } else {
-    img1.classList.remove("highlight", "grayed-out");
-    img2.classList.remove("highlight", "grayed-out");
-  }
-}
 
 const emailInput = userForm?.elements.namedItem("email") as HTMLInputElement;
 if (emailInput) {
